a84789e813a3c3e0d99a0ce123a474cfe4ed2c6b
Will O'Leary <wolearyc@gmail.com>
2014-02-16 13:53:52 -0800

    Documentation

--- WickEngine_SDK/include/Bounds.h
+++ WickEngine_SDK/include/Bounds.h
@@ -22,20 +22,72 @@
 #include "Pair.h"
 namespace wick
 {
+    /// \brief Defines a rectangular area.
+    ///
+    /// Bounds is most commonly used when defining images that only use a
+    /// a specific rectangular portion of a texture. 
     class Bounds
     {
     public:
+        /// \brief A constructor taking two Pairs.
+        ///
+        /// Each Pair represents an x-y coordinate. The Pairs do not 
+        /// necessarly need to define the lower-left or upper-right x-y 
+        /// coordinate of the bounding rectangle, but if they do not, the
+        /// resulting bounds will be inverted.
+        /// \param lowerLeft the lower-left x-y coordinate of the bounding 
+        ///        rectangle
+        /// \param upperRight the upper-right x-y coordinate of the bounding
+        ///        rectangle
         Bounds(Pair lowerLeft, Pair upperRight);
+        /// \brief A constructor taking four ints.
+        ///
+        /// \param lowerLeftX x value of the lower-left x-y coordinate
+        /// \param lowerLeftY y value of the lower-left x-y coordinate
+        /// \param upperRightX x value of the upper-right x-y coordinate
+        /// \param upperRightY y value of the upper-right x-y coordinate
         Bounds(int lowerLeftX, int lowerLeftY, int upperRightX, int upperRightY);
+        /// \brief A constructor taking a location and dimensions.
+        ///
+        /// \param location the location of the lower-left x-y coordinate
+        /// \xDimension width of the bounding rectangle
+        /// \yDimension height of the bounding rectangle
         Bounds(Pair location, int xDimension, int yDimension);
+        /// \brief Default constructor.
         Bounds();
+        /// \brief Copy constructor.
         Bounds(const Bounds& other);
+        /// \brief Gets the lower-left x-y coordinate.
+        ///
+        /// \return lower-left x-y coordinate
         Pair getLowerLeft();
+        /// \brief Sets the lower-left x-y coordinate.
+        ///
+        /// \param lowerLeft desired lower-left x-y coordinate
         void setLowerLeft(Pair lowerLeft);
+        /// \brief Translates the lower-left x-y coordinate.
+        ///
+        /// \param translation desired translation of the lower-left 
+        ///        x-y coordinate
         void translateLowerLeft(Pair translation);
+        /// \brief Gets the upper-right x-y coordinate.
+        ///
+        /// \return upper-right x-y coordinate
         Pair getUpperRight();
+        /// \brief Sets the upper-right x-y coordinate.
+        ///
+        /// \param upperRight desired upper-right x-y coordinate
         void setUpperRight(Pair upperRight);
+        /// \brief Translates the upper-right x-y coordinate.
+        ///
+        /// \param translation desired translation of the upper-right 
+        ///        x-y coordinate
         void translateUpperRight(Pair translation);
+        /// \brief Sets the lower-left and upper-right x-y coordinates
+        ///        simultaneously.
+        ///
+        /// \param lowerLeft desired lower-left x-y coordinate
+        /// \param upperRight desired upper-right x-y coordinate
         void setLowerLeftAndUpperRight(Pair lowerLeft, Pair upperRight);
     protected:
         Pair lowerLeft_;
