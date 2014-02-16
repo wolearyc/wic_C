ec4f61372da4a878361f01e11fffff9c411cb9f7
Will O'Leary <wolearyc@gmail.com>
2014-02-16 15:34:57 -0800

    Documentation

--- WickEngine_SDK/include/Font.h
+++ WickEngine_SDK/include/Font.h
@@ -43,16 +43,64 @@ using std::string;
 using std::vector;
 namespace wick
 {
+    /// \brief Loads and stores a font.
+    ///
+    /// Font loads a font file from memory and defines said font at a certain 
+    /// point size. TrueType fonts (TTF), TrueType collections (TTC), Type 1 
+    /// fonts(PFA and PFB), CID-keyed Type 1 fonts, CFF fonts, OpenType fonts,
+    /// OpenType collections, SFNT-based bitmap fonts, X11 PCF fonts, Windows
+    /// FNT fonts, BDF fonts, and PFR fonts are supported. There is also 
+    /// limited support for Type 42 fonts. Font should be initialized as a 
+    /// pointer (using "new") and destroyed after use. 
+    ///
+    /// Regarding antialiasing: Usually, smaller fonts are blurry when
+    /// antialiasing is turned on. Therefore, it is best to disable antialiasing
+    /// when rendering small fonts.
     class Font
     {
     public:
+        /// \brief A constructor.
+        ///
+        /// \param filePath absolute or relative filepath to the font file
+        /// \param point size of the font in font points
+        /// \param game the game (necessary in order to define screen resolution)
+        /// \param antialias whether or not the font should be antialiased
+        /// \exception WickException fatal exception when the font file existing
+        ///            at filePath cannot be found, is currupted, or is not 
+        ///            compatible with Wick.
         Font(string filePath, unsigned short point, Game* game, bool antialias);
+        /// \brief A constructor defining an antialiased font.
+        ///
+        /// \param filePath absolute or relative filepath to the font file
+        /// \param point size of the font in font points
+        /// \param game the game (necessary in order to define screen resolution)
+        /// \exception WickException fatal exception when the font file existing
+        ///            at filePath cannot be found, is currupted, or is not 
+        ///            compatible with Wick.
         Font(string filePath, unsigned short point, Game* game);
+        /// \brief Default constructor.
+        ///
+        /// This constructor does NOT create a complete Font object. There is
+        /// no "default" font that can be used to paint Text.
         Font();
+        /// \brief Copy constructor.
         Font(const Font& other);
+        /// \brief Destructor.
         ~Font();
+        /// \brief Gets the size of the font.
+        ///
+        /// \return the font size in font points
         unsigned short getPoint();
+        /// \brief Modifies the size of the font.
+        ///
+        /// \param point desired font size in font points
         void setPoint(unsigned short point);
+        /// \brief Gets a set of Images rendering a certain string in the font.
+        /// 
+        /// \message the string to be rendered
+        /// \return a set of Images rendering message (all of the Images's 
+        ///         locations are (0,0), but their centers define the offset of
+        ///         each character)
         vector<Image> getImages(string message);
     private:
         FT_Library library_;
