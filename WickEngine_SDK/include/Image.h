bc2138cc9da57347e2687084345c6997f4e7c2d9
Will O'Leary <wolearyc@gmail.com>
2014-02-16 15:44:48 -0800

    Documentation

--- WickEngine_SDK/include/Image.h
+++ WickEngine_SDK/include/Image.h
@@ -25,16 +25,47 @@
 #include "WickException.h"
 namespace wick
 {
+    /// \brief An Image to be painted to the screen.
+    ///
+    /// An Image is defined by a Texture, and can be moved, rotated, scaled, and 
+    /// centered. In essense, an Image behaves just like Quad. Multiple Image 
+    /// objects can be defined by the same Texture.
     class Image : public Quad
     {
     public:
+        /// \brief A constructor.
+        ///
+        /// Constructs an unrotated and unscaled Image.
+        /// \param location the screen location
+        /// \param texture the texture
         Image(Pair location, Texture* texture);
+        /// \brief Default constructor.
+        ///
+        /// This constructor does NOT construct a valid, paintable Image.
         Image();
+        /// \brief Copy constructor.
         Image(const Image& other);
+        /// \brief Paints the Image to the screen.
+        ///
+        /// \param game the game
         void paint(Game* game);
+        /// \brief Gets the current Texture.
+        ///
+        /// \return the current Texture
         Texture* getTexture();
+        /// \brief Changes the current Texture.
+        ///
+        /// \param texture the desired new Texture
         void setTexture(Texture* texture);
+        /// \brief Returns the current Texture bounds
+        ///
+        /// \return the rectangular bounds of the Texture currently in use
         Bounds getBounds();
+        /// \brief Modifies the current Texture bounds
+        ///
+        /// \param bounds the desired rectanglar bounds of the Texture to use
+        /// \exception WickException non-fatal exception when the bounds 
+        ///            go beyond the Texture's dimensions
         void setBounds(Bounds bounds);
     protected:
         Texture* texture_;
