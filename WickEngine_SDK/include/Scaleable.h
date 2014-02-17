f2e7358b2bd23a49f7dbe9091675c77c25d77c52
Will O'Leary <wolearyc@gmail.com>
2014-02-16 17:54:09 -0800

    Documentation

--- WickEngine_SDK/include/Scaleable.h
+++ WickEngine_SDK/include/Scaleable.h
@@ -22,14 +22,36 @@
 #include "Pair.h"
 namespace wick
 {
+    /// \brief An architectural superclass that should by inherited by any 
+    ///        class that can be scaled.
+    ///
+    /// Any class that inherits from Scaleable can be scaled. The object is
+    /// rotated around a center, which is defined in Paintable. A scale value
+    /// is a multiplier of the object's side.
     class Scaleable
     {
     public:
+        /// \brief A constructor.
+        ///
+        /// \param scale the scale
         Scaleable(Pair scale);
+        /// \brief Default constructor.
+        ///
+        /// This constructor constructs an unscaled object.
         Scaleable();
+        /// \brief Copy constructor.
         Scaleable(const Scaleable& other);
+        /// \brief Gets the scale.
+        ///
+        /// \return the scale
         Pair getScale();
+        /// \brief Modifies the scale.
+        ///
+        /// \param scale the desired scale
         void setScale(Pair scale);
+        /// \brief Scales the object.
+        ///
+        /// \param scale the value to add to the current scale
         void scale(Pair scale);
     protected:
         Pair scale_;
