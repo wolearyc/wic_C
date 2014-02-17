ad584b88535acb9cf0b166e0003738c1b3425d0d
Will O'Leary <wolearyc@gmail.com>
2014-02-16 17:35:43 -0800

    Documentation

--- WickEngine_SDK/include/Polygon.h
+++ WickEngine_SDK/include/Polygon.h
@@ -30,21 +30,58 @@ using std::initializer_list;
 using std::vector;
 namespace wick
 {
+    /// \brief A convex polygon that can be painted to the screen.
+    ///
+    /// A Polygon can be painted, rotated, and scaled.
     class Polygon : public Paintable, public Rotateable, public Scaleable
     {
     public:
-
-
+        /// \brief A constructor taking a vector of vertices.
+        ///
+        /// \param location the screen location
+        /// \param baseVertices the set of vertices (relative to object)
+        /// \param color the color
         Polygon(Pair location, vector<Pair> baseVertices, Color color);
+        /// \brief A constructor taking an initializer_list of vertices.
+        ///
+        /// This constructor allows the vertices to be enclosed in curly
+        /// braces ({Pair a, Pair b, etc.}).
+        /// \param location the screen location
+        /// \param baseVertices the set of vertices (relative to object)
+        /// \param color the color
         Polygon(Pair location, initializer_list<Pair> baseVertices,
                 Color color);
+        /// \brief Default constructor.
+        ///
+        /// Constructs a Polygon with 0 vertices.
         Polygon();
+        /// \brief Copy constructor.
         Polygon(const Polygon& other);
+        /// \brief Paints the Polygon to the screen.
+        ///
+        /// \param game the game
         void paint(Game* game);
+        /// \brief Gets the vertices.
+        ///
+        /// \return the set of vertices
         vector<Pair> getBaseVertices();
+        /// \brief Modifies the vertices.
+        ///
+        /// \param baseVertices the desired set of vertices
         void setBaseVertices(vector<Pair> baseVertices);
+        /// \brief Modifies the vertices.
+        ///
+        /// The use of initializer_list allows the set of new vertices to be enclosed
+        /// in curly braces ({Pair a, Pair b, etc.}).
+        /// \param baseVertices the desired set of vertices
         void setBaseVertices(initializer_list<Pair> baseVertices);
+        /// \brief Gets the color.
+        ///
+        /// \return the color
         Color getColor();
+        /// \brief Modifies the color.
+        ///
+        /// \param color the desired color
         void setColor(Color color);
     protected:
         vector<Pair> baseVertices_;
